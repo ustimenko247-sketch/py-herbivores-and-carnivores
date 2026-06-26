09539949399b2372056e23156cdf1142e11a3de3
Your Name <denys@mate.academy>
2026-06-26 14:32:39 +0300

    Fix style issues and pass all tests

--- app/main.py
+++ app/main.py
@@ -1,1 +1,32 @@
-# write your code here
+class Animal:
+    alive = []
+
+    def __init__(self, name: str, health: int = 100) -> None:
+        self.name = name
+        self.health = health
+        self.hidden = False
+        Animal.alive.append(self)
+
+    def __repr__(self) -> str:
+        return (
+            f"{{Name: {self.name}, "
+            f"Health: {self.health}, "
+            f"Hidden: {self.hidden}}}"
+        )
+
+
+class Herbivore(Animal):
+    def hide(self) -> None:
+        self.hidden = not self.hidden
+
+
+class Carnivore(Animal):
+    def bite(self, herbivore: "Herbivore") -> None:
+        if (
+            isinstance(herbivore, Herbivore)
+            and not herbivore.hidden
+            and herbivore.health > 0
+        ):
+            herbivore.health -= 50
+            if herbivore.health <= 0:
+                Animal.alive.remove(herbivore)
